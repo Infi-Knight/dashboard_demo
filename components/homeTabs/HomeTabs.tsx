886f65b22ea0d6779d5fba79e3c7e2c4f983bfe7
Ravi Soni <soniravi829@gmail.com>
2022-01-01 08:12:59 +0530

    some perf optimizations and table ui

--- components/homeTabs/HomeTabs.tsx
+++ components/homeTabs/HomeTabs.tsx
@@ -10,10 +10,57 @@ import { LinkButton } from '@/components/button';
 import { filterTabId } from '@/components/filter/Filter';
 
 import NewInvoiceIcon from '@/icons/new_invoice_icon.svg';
-import Table from '../table';
+import Table from '@/components/table';
 // TODO: https://css-tricks.com/bold-on-hover-without-the-layout-shift/
 // fix the shift due to font weight change on tab change
 export const HomeTabs = (): JSX.Element => {
+  return (
+    <Tabs>
+      {({ selectedIndex, focusedIndex }) => {
+        let getTabStyle = (index: number) => {
+          if (selectedIndex === index) {
+            // active tab
+            return `text-primary-blue font-bold border-b-4 border-primary-blue`;
+          } else {
+            return `text-gray-600 border-b-4 border-transparent`;
+          }
+        };
+        return (
+          <React.Fragment>
+            <TabList className="border-b border-gray-200 bg-body">
+              <Tab className={`px-2.5 py-4 ${getTabStyle(0)}`}>Invoices</Tab>
+              <Tab className={`px-2.5 py-4 ${getTabStyle(1)}`}>Drafts</Tab>
+            </TabList>
+
+            <TabPanels>
+              <TabPanel>
+                <InvoicesPanelHeader />
+                <InvoicesPanelBody />
+              </TabPanel>
+
+              <TabPanel>Coming soon...</TabPanel>
+            </TabPanels>
+          </React.Fragment>
+        );
+      }}
+    </Tabs>
+  );
+};
+
+const InvoicesPanelBody = React.memo(function InvoicesPanelBody() {
+  return (
+    <>
+      {/* this div makes space for filter tab when it is opened */}
+      {/* this div's height is equal to the height of filter tab */}
+      <div id="phantom-div" className="hidden md:block md:mt-4 lg:mt-6"></div>
+      <div className="mx-auto mt-8 max-w-screen-xl">
+        <Table />
+      </div>
+    </>
+  );
+});
+
+const InvoicesPanelHeader = React.memo(function InvoicesPanelHeader() {
   const [isFilterTabOpen, setIsFilterTabOpen] = React.useState<boolean>(false);
 
   // TODO: clean up the code and fix any perf issues in this handler and associated useEffect down below
@@ -63,68 +110,31 @@ export const HomeTabs = (): JSX.Element => {
   }, [isFilterTabOpen, handlePhantomDivResize]);
 
   return (
-    <Tabs>
-      {({ selectedIndex, focusedIndex }) => {
-        let getTabStyle = (index: number) => {
-          if (selectedIndex === index) {
-            // active tab
-            return `text-primary-blue font-bold border-b-4 border-primary-blue`;
-          } else {
-            return `text-gray-600 border-b-4 border-transparent`;
-          }
-        };
-        return (
-          <React.Fragment>
-            <TabList className="border-b border-gray-200 bg-body">
-              <Tab className={`px-2.5 py-4 ${getTabStyle(0)}`}>Invoices</Tab>
-              <Tab className={`px-2.5 py-4 ${getTabStyle(1)}`}>Drafts</Tab>
-            </TabList>
+    <div className="relative flex flex-wrap items-end mt-6 gap-4">
+      <div className="order-4 w-full mt-4 md:order-1 md:basis-[530px] md:grow lg:basis-0">
+        {/* this div makes space for filter tab when it is opened */}
+        {/* this div's height is equal to the height of filter tab */}
+        <div id="phantom-div-2" className="md:hidden"></div>
+        <ClubSelector
+          id="invoice"
+          clubs={['AFC Eskilstuna', 'AIK Atlas', 'Adolfsbergs IK']}
+          defaultClub="AFC Eskilstuna"
+        />
+      </div>
 
-            <TabPanels>
-              <TabPanel>
-                <div className="relative flex flex-wrap items-end mt-6 gap-4">
-                  <div className="order-4 w-full mt-4 md:order-1 md:basis-[530px] md:grow lg:basis-0">
-                    {/* this div makes space for filter tab when it is opened */}
-                    {/* this div's height is equal to the height of filter tab */}
-                    <div id="phantom-div-2" className="md:hidden"></div>
-                    <ClubSelector
-                      id="invoice"
-                      clubs={['AFC Eskilstuna', 'AIK Atlas', 'Adolfsbergs IK']}
-                      defaultClub="AFC Eskilstuna"
-                    />
-                  </div>
+      <div className="order-3 md:order-4 lg:order-2">
+        <Filter setIsFilterTabOpen={setIsFilterTabOpen} />
+      </div>
 
-                  <div className="order-3 md:order-4 lg:order-2">
-                    <Filter setIsFilterTabOpen={setIsFilterTabOpen} />
-                  </div>
+      <div className="order-2 grow md:min-w-[351px] md:grow-0 md:order-3">
+        <Search />
+      </div>
 
-                  <div className="order-2 grow md:min-w-[351px] md:grow-0 md:order-3">
-                    <Search />
-                  </div>
-
-                  <div className="order-1 w-full md:order-2 md:w-auto lg:order-4">
-                    <Link href="#" passHref>
-                      <LinkButton Icon={NewInvoiceIcon}>New Invoice</LinkButton>
-                    </Link>
-                  </div>
-                </div>
-
-                {/* this div makes space for filter tab when it is opened */}
-                {/* this div's height is equal to the height of filter tab */}
-                <div
-                  id="phantom-div"
-                  className="hidden md:block md:mt-4 lg:mt-6"
-                ></div>
-                <div className="mt-8">
-                  <Table />
-                </div>
-              </TabPanel>
-
-              <TabPanel>Coming soon...</TabPanel>
-            </TabPanels>
-          </React.Fragment>
-        );
-      }}
-    </Tabs>
+      <div className="order-1 w-full md:order-2 md:w-auto lg:order-4">
+        <Link href="#" passHref>
+          <LinkButton Icon={NewInvoiceIcon}>New Invoice</LinkButton>
+        </Link>
+      </div>
+    </div>
   );
-};
+});
--- components/table/Table.tsx
+++ components/table/Table.tsx
@@ -4,9 +4,14 @@ import matchSorter from 'match-sorter';
 
 import { invoiceStatuses } from '@/config/index';
 import { invoiceStatusUiData } from '@/components/filter/Filter';
-import { InvoiceStatus } from '../../types';
+import { InvoiceStatus } from '@/types/index';
+import EditIcon from '@/icons/edit_icon.svg';
+import PdfIcon from '@/icons/pdf_icon.svg';
+import { LinkButton } from '@/components/button';
+import { getCheckboxStyles } from '@/components/filter/utils';
 
 faker.setLocale('sv');
+
 const range = (len: number): Array<any> => {
   const arr = [];
   for (let i = 0; i < len; i++) {
@@ -46,12 +51,13 @@ const currencyFormatter = new Intl.NumberFormat(undefined, {
   style: 'currency',
   currency: 'SEK',
 });
-const Table = (): JSX.Element => {
+
+const Table = React.memo(function Table() {
   const data = React.useMemo(() => {
-    return range(100).map((_, index) => {
+    return range(10).map((_, index) => {
       return {
         serialNumber: index + 1,
-        name: faker.name.findName(),
+        name: faker.company.companyName(),
         // date: new Intl.DateTimeFormat('en-US').format(faker.date.past()),
         // dueDate: new Intl.DateTimeFormat('en-US').format(faker.date.future()),
         date: faker.date.past(),
@@ -63,6 +69,7 @@ const Table = (): JSX.Element => {
     });
   }, []);
 
+  // TODO: fix responsiveness for the table on > 1280px screens
   return (
     <section role="grid" className="bg-white border-0 lg:border lg:rounded">
       <div
@@ -71,7 +78,7 @@ const Table = (): JSX.Element => {
       >
         <div
           role="row"
-          className="font-semibold text-gray-800 grid grid-cols-8 gap-x-6 font-sm"
+          className="font-semibold text-gray-800 grid grid-cols-[2rem_20rem_6rem_6rem_8rem_10rem_3.4375rem_3.4375rem] gap-x-6 font-sm"
         >
           <span role="columnheader">Nº</span>
           <span role="columnheader">Name</span>
@@ -79,8 +86,12 @@ const Table = (): JSX.Element => {
           <span role="columnheader">Due date</span>
           <span role="columnheader">Amount</span>
           <span role="columnheader">Status</span>
-          <span role="columnheader">PDF</span>
-          <span role="columnheader">Edit</span>
+          <span role="columnheader" className="justify-self-center">
+            PDF
+          </span>
+          <span role="columnheader" className="justify-self-center">
+            Edit
+          </span>
         </div>
       </div>
       <div role="presentation">
@@ -89,28 +100,74 @@ const Table = (): JSX.Element => {
             <div
               role="row"
               key={`${name + date + amount}`}
-              className="grid grid-cols-8 gap-x-6 px-4 md:px-6 pt-3.5 pb-[0.9375rem] text-sm border-b border-gray-200"
+              className="grid items-center grid-cols-[2rem_20rem_6rem_6rem_8rem_10rem_3.4375rem_3.4375rem] gap-x-6 px-4 md:px-6 pt-3.5 pb-[0.9375rem] text-sm border-b border-gray-200"
             >
               <span role="gridcell">{serialNumber}</span>
               <span role="gridcell" className="font-medium">
                 {name}
               </span>
-              <span role="gridcell">
-                {currencyFormatter.format(parseFloat(amount))}
-              </span>
               <span role="gridcell">{getFormattedDate(date)}</span>
+              <span role="gridcell">{getFormattedDate(dueDate)}</span>
               <span role="gridcell">
                 {currencyFormatter.format(parseFloat(amount))}
               </span>
-              <span role="gridcell">{status}</span>
-              <span role="gridcell">pdf</span>
-              <span role="gridcell">edit</span>
+              <span role="gridcell">
+                <StatusBadge status={status} />
+              </span>
+              <span role="gridcell" className="justify-self-center">
+                <LinkButton
+                  className="pt-2 pb-2 pl-2 pr-2 bg-white border-0"
+                  iconColor="text-primary-blue"
+                  href="#"
+                  Icon={PdfIcon}
+                />
+              </span>
+              <span role="gridcell" className="justify-self-center">
+                <LinkButton
+                  className="pt-2 pb-2 pl-2 pr-2 bg-white border-0"
+                  iconColor="text-primary-blue"
+                  href="#"
+                  Icon={EditIcon}
+                />
+              </span>
             </div>
           );
         })}
       </div>
     </section>
   );
-};
+});
 
+const StatusBadge = React.memo(function StatusBadge({
+  status,
+}: {
+  status: InvoiceStatus;
+}) {
+  const {
+    statusName,
+    icon: Icon,
+    color,
+  } = React.useMemo(
+    () => invoiceStatusUiData[status],
+    [status, invoiceStatusUiData]
+  );
+  const { labelBorder, labelBg, svgColor } = React.useMemo(
+    () => getCheckboxStyles(color),
+    [getCheckboxStyles, color]
+  );
+
+  const labelStyles = `select-none inline-flex items-center ${labelBg} p-1 pl-2.5 pr-3 border rounded-[20px] text-gray-800 cursor-pointer ${labelBorder}`;
+  const iconStyles = `${svgColor} mr-2.5`;
+
+  return (
+    <div className={labelStyles}>
+      <span className={iconStyles}>
+        <Icon />
+      </span>
+      <span className="text-xs font-medium text-gray-800 font-montserrat">
+        {statusName}
+      </span>
+    </div>
+  );
+});
 export default Table;
